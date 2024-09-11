118f51048b5a085e1e0d5a224c5ede7aef4ecb05
Mohamed Osama <mohamed.osama@booking.com>
2024-09-11 23:44:51 +0200

    change endpoint url for today-hadith api

--- server/hadith_app/routes/user_routes.py
+++ server/hadith_app/routes/user_routes.py
@@ -12,28 +12,58 @@ logger = logging.getLogger(__name__)
 @app.route('/')
 def index():
     """
-       Route for the index page.
+        Attempts to fetch the hadith of the day using the helper function.
 
-       This function handles the rendering of the index page, which displays today's hadith.
+        Returns:
+            dict: A dictionary containing either the hadith of the day or an error message,
+                  along with the corresponding HTTP status code.
+    """
+    result = _try_get_today_hadith()
+    if result.get('today_hadith') is not None:
+        return render_template("index.html", today_hadith=result.get('today_hadith'))
+    else:
+        return render_template("index.html", error=result.get('error')), result.get('status_code')
 
-       Returns:
-           str: Rendered HTML template for the index page.
 
-       Raises:
-           ValueError: If there is an error with the provided data.
-           KeyError: If the required data is not found.
-           Exception: If there is an internal server error.
+@app.route('/api/today-hadith')
+@cross_origin()
+def get_hadith_of_the_day():
+    """
+    Retrieves the hadith of the day.
 
-       Note:
-           If an error occurs, the function will render the index template with an error message.
+    This function attempts to fetch the hadith of the day using the _try_get_today_hadith() helper function.
+    If successful, it returns the hadith. If an error occurs, it returns a JSON response with an error message
+    and the appropriate HTTP status code.
+
+    Returns:
+        dict or tuple: If successful, returns the hadith of the day as a dictionary.
+                       If an error occurs, returns a tuple containing a JSON response with an error message
+                       and the corresponding HTTP status code.
+    """
+    result = _try_get_today_hadith()
+    if result.get('today_hadith') is not None:
+        return result.get('today_hadith')
+    else:
+        return jsonify(error=result.get('error')), result.get('status_code')
+
+
+def _try_get_today_hadith():
+    """
+    Attempts to fetch the hadith of the day.
+
+    This function wraps the `get_today_hadith()` call with error handling.
+    It catches various exceptions that might occur during the process and
+    returns appropriate error messages and status codes.
+
+    Returns:
+        dict: A dictionary containing either the hadith of the day or an error message,
+              along with the corresponding HTTP status code.
     """
     try:
         today_hadith = get_today_hadith()
-        if today_hadith is None:
-            error_message = "Hadith not found"
-            status_code = 404
-        else:
-            return render_template("index.html", today_hadith=today_hadith)
+        if today_hadith is not None:
+            return {"today_hadith": today_hadith, "status_code": 200}
+        return {"error": "Hadith not found", "status_code": 404}
     except ValueError as ve:
         logger.error(f"Value error: {ve}")
         error_message = "Something went wrong"
@@ -46,36 +76,4 @@ def index():
         logger.error(f"Error fetching today's hadith: {e}")
         error_message = "Something went wrong. Please try again later."
         status_code = 500
-    return render_template("index.html", error=error_message), status_code
-
-
-# TODO to be moved
-@app.route('/today-hadith')
-@cross_origin()
-def get_hadith_of_the_day():
-    """
-    Route to fetch today's hadith.
-
-    Returns:
-        Response: A JSON response with the hadith details if found, otherwise an error message.
-
-    Raises:
-        ValueError: If there is an error with the provided data.
-        KeyError: If the required data is not found.
-        Exception: If there is an internal server error.
-    """
-    try:
-        today_hadith = get_today_hadith()
-        if today_hadith is not None:
-            return today_hadith
-        else:
-            return jsonify(error="Hadith not found"), 404
-    except ValueError as ve:
-        logger.error(f"Value error: {ve}")
-        return jsonify(error="Invalid data provided"), 400
-    except KeyError as ke:
-        logger.error(f"Key error: {ke}")
-        return jsonify(error="Data not found"), 404
-    except Exception as e:
-        logger.error(f"Error fetching today's hadith: {e}")
-        return jsonify(error="Internal server error"), 500
+    return {"error": error_message, "status_code": status_code}
