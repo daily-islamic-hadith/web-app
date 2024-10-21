66af358087d8fd461f8a4aac75be283bbd4052d6
Mohamed Osama <mohamed.osama@booking.com>
2024-10-22 01:56:42 +0300

    add new button to let the user ask for another hadith

--- server/hadith_app/static/js/scripts.js
+++ server/hadith_app/static/js/scripts.js
@@ -4,7 +4,44 @@ document.addEventListener('DOMContentLoaded', () => {
     if (copy_button) {
         copy_button.addEventListener('click', copyToClipboard);
     }
+    const show_new_hadith_button = document.querySelector('#showNewHadith');
+    if (show_new_hadith_button) {
+        show_new_hadith_button.addEventListener('click', fetchNewHadith);
+    }
 });
+
+async function fetchNewHadith() {
+    const url = window.location.href + `/api/fetch-hadith?fetch-mode=random`;
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const hadith = await response.json();
+        if (hadith) {
+            const quoteEnglish = hadith.hadithEnglish || 'English version not available';
+            const quoteArabic = hadith.hadithArabic || 'Arabic version not available';
+            const bookName = hadith.book?.bookName || 'Book name not available';
+            const writerName = hadith.book?.writerName || 'Writer name not available';
+            const source = `Source: ${bookName} by ${writerName}`;
+            document.getElementById('quoteEnglish').textContent = quoteEnglish;
+            document.getElementById('quoteArabic').textContent = quoteArabic;
+            document.getElementById('source').textContent = source;
+
+            // Show the copy button after successful fetch
+            document.getElementById('copyButton').style.display = 'flex';
+        } else {
+            document.getElementById('quoteEnglish').textContent = 'No hadith found.';
+            document.getElementById('quoteArabic').textContent = '';
+            document.getElementById('source').textContent = '';
+        }
+    } catch (error) {
+        console.error('Failed to fetch the hadith:', error);
+        document.getElementById('quoteEnglish').textContent = 'Failed to fetch hadith. Please try again later.';
+        document.getElementById('quoteArabic').textContent = '';
+        document.getElementById('source').textContent = '';
+    }
+}
 
 function detectBrowser() {
     const userAgent = navigator.userAgent;
